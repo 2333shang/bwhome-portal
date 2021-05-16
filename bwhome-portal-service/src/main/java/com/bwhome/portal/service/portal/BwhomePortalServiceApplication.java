5cc4e235ed88f46c6a270a14c352feacc718dd5f
2333shang <1152761842@qq.com>
2021-05-16 22:18:50 +0800

    init project

---
+++ bwhome-portal-service/src/main/java/com/bwhome/portal/service/portal/BwhomePortalServiceApplication.java
@@ -0,0 +1,13 @@
+package com.bwhome.portal.service.portal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BwhomePortalServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BwhomePortalServiceApplication.class, args);
+    }
+
+}
