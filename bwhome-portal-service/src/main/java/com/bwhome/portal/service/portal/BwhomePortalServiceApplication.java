d45ec9d7c33a9ba1dfd765597a009c68454a97ef
sfzk <472252050@qq.com>
2021-06-22 17:31:34 +0800

    目录结构

--- bwhome-portal-service/src/main/java/com/bwhome/portal/service/portal/BwhomePortalServiceApplication.java
+++ bwhome-portal-service/src/main/java/com/bwhome/portal/service/portal/BwhomePortalServiceApplication.java
@@ -2,8 +2,12 @@ package com.bwhome.portal.service.portal;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.ComponentScan;
 
+@EnableDiscoveryClient
+@EnableFeignClients
 @SpringBootApplication
 public class BwhomePortalServiceApplication {
 
