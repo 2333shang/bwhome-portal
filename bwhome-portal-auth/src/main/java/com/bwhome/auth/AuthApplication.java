7087767c752b674afcaa1e787ca14cfdf8fb8ac6
sfzk <472252050@qq.com>
2021-06-24 15:16:31 +0800

    跨模块调用fegin，指明包路径

--- bwhome-portal-auth/src/main/java/com/bwhome/auth/AuthApplication.java
+++ bwhome-portal-auth/src/main/java/com/bwhome/auth/AuthApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
-@EnableFeignClients
+@EnableFeignClients(basePackages = "com.bwhome.api.system.service")
 @SpringBootApplication
 public class AuthApplication {
     public static void main(String[] args) {
